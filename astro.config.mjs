ef84a3b5469043716dc8d03856abde831ccb7707
Santiago Muñoz Castro <santiyagito22@gmail.com>
2024-10-03 02:20:34 +0200

    Deploy try v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
@@ -11,13 +12,5 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  vite: {
-    resolve: {
-      alias: {
-        '@': resolve('./src')
-      }
-    }
-  },
   site: 'https://smuncastro.dev',
-
 });
